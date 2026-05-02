ad8cde6fdbe23dffa9ba0a03c8b3e8d752654873
Yashwanth Singh <yashwanthsingh.552001@gmail.com>
2026-05-02 13:00:52 +0530

    fix: update BASE_URL for production

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Apex-AI-Builder/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://oracleapex.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/ords/yash_tt/ai-builder'),
-      },
-    },
-  },
-});
+})
